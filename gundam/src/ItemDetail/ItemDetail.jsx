f8c19e9a6c45c7c7a76f0afef4b0b25294fc8650
hwang-jiho <ghkdwlgh93@naver.com>
2024-06-19 19:30:47 +0900

    최초 commit

--- gundam/src/ItemDetail/ItemDetail.jsx
+++ gundam/src/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+
 export default function ItemDetail () {
     return (
-        <div className="ItemDetail" style={{height:'100vh', width:'100vw'}}>
-                asdf
+        <div className="item_detail_main">
+            <div className='detail_img_box'>
+                <img className="item_detail_img" src='./image/gundam_Logo_1.jpg' />
+            </div>
+            <div className='detail_right_box'>
+                
+            </div>
         </div>
     )
 }

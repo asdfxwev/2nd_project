a97e3d521f4791387a517c8d3ca3190f773a3bc2
Vaundy0824 <167267377+Vaundy0824@users.noreply.github.com>
2024-06-20 12:58:16 +0900

    ㅇㅅㅇ

--- gundam/src/ItemList/ItemList.jsx
+++ gundam/src/ItemList/ItemList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ItemCard from './ItemCard';
-import './ItemList.css';
+import { Link } from 'react-router-dom';
+import './ItemList.css'
 
 const ItemList = ({ items }) => {
     return (
-        <div className="item-list">
-            {items.map(item => (
-                <ItemCard key={item.id} Item={item} />
-            ))}
+        <div>
+            <h2>Item List</h2>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        <Link to={`/ItemDetail/${item.id}`}>{item.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
--- gundam/src/main/Main.js
+++ gundam/src/main/Main.js
@@ -2,6 +2,8 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 import MainComponent from './MainComponent';
 import ItemList from '../ItemList/ItemList';
 import ItemDetail from '../ItemDetail/ItemDetail';
+import ItemDataBase from '../ItemList/ItemDataBase'
+import React,{useState, useEffect} from 'react'
 
 export default function Main() {
     const [items, setItems] = useState([]);

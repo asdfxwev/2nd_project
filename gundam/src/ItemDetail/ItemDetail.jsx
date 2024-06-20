3df4fa1c7ed92e258b9df6a666aec6bc4e945a27
hwang-jiho <ghkdwlgh93@naver.com>
2024-06-20 16:24:28 +0900

    임시

--- gundam/src/ItemDetail/ItemDetail.jsx
+++ gundam/src/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import './ItemDetail.css'
+import '../App.css';
 
 export default function ItemDetail () {
     return (
         <div className="item_detail_main">
             <div className='detail_img_box'>
-                <img className="item_detail_img" src='./image/gundam_Logo_1.jpg' />
+                <img className="item_detail_img" src='./image/tabImage14.jpg' />
             </div>
             <div className='detail_right_box'>
-                
+                <div className='detail_top'>좋아요아이콘용</div>
+                <div className='item_name'>건담이름</div>
+                <div className='item_info'>상품정보</div>
             </div>
         </div>
     )

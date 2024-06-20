af052ef31b2bb0478b3e2055419e5ea5d6d62715
hwang-jiho <ghkdwlgh93@naver.com>
2024-06-20 17:55:09 +0900

    임시

--- gundam/src/ItemDetail/ItemDetail.jsx
+++ gundam/src/ItemDetail/ItemDetail.jsx
@@ -11,7 +11,11 @@ export default function ItemDetail () {
             <div className='detail_right_box'>
                 <div className='detail_top'>좋아요아이콘용</div>
                 <div className='item_name'>건담이름</div>
+                <div className='item_price'>금액</div>
                 <div className='item_info'>상품정보</div>
+                <div className='item_count'>구매수량</div>
+                <div className='item_totar_price'>총금액</div>
+                <div></div>
             </div>
         </div>
     )

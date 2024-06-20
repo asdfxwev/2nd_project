2260eacdbbd8c5c8a31e50033db7ec1f6decdea5
Vaundy0824 <167267377+Vaundy0824@users.noreply.github.com>
2024-06-20 16:25:37 +0900

    소소한 수정

--- gundam/src/ItemList/ItemList.jsx
+++ gundam/src/ItemList/ItemList.jsx
@@ -7,6 +7,14 @@ const ItemList = () => {
 
     return (
         <div className="item-list">
+            <div className='item-choose'>
+                <div>필터 숨기기 </div>
+                <div>선택된 필터: <button>초기화</button></div>
+                <span>상품정보</span>
+                <span>가격대별</span>
+                <span>브랜드별</span>
+                <span>작품별</span>
+            </div>
             {ItemDataBase.map(item => (
                 <ItemCard key={item.id} item={item} />
             ))}

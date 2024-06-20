30356b22ab5ed4bcf03f516d115bc5eecfd56516
dydejr <167267452+dydejr@users.noreply.github.com>
2024-06-20 14:40:33 +0900

    1차 몸부림

--- gundam/src/App.js
+++ gundam/src/App.js
@@ -4,6 +4,7 @@ import Main from './main/Main';
 import ItemList from './ItemList/ItemList';
 import ItemDetail from './ItemDetail/ItemDetail';
 import { Routes, Route, NavLink } from 'react-router-dom';
+import Footer from './Footer/Footer';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Footer/>
     </div>
   );
 }

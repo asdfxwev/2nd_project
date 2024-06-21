1dc2a2995d13009ab1ddb1de36a3f03770d8f87a
dydejr <167267452+dydejr@users.noreply.github.com>
2024-06-21 14:51:07 +0900

    1차 로그인

--- gundam/src/App.js
+++ gundam/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Header from './header/Header'
 import Main from './main/Main';
-import Footer from './Footer/Footer'
+import Footer from './Footer/Footer';
 import ItemList from './ItemList/ItemList';
 import ItemDetail from './ItemDetail/ItemDetail';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Footer from './Footer/Footer';
 
 
 
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-
       <Main />
       <Footer />
     </div>

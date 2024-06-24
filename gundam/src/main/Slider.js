1a9d0ece0f4aa9f95dba2c78b6965285cd4bc59e
asdfxwev <167267637+asdfxwev@users.noreply.github.com>
2024-06-24 23:54:13 +0900

    as

--- gundam/src/main/Slider.js
+++ gundam/src/main/Slider.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './slider.css';
+// import './slider.css';
 import ImageSlider, { Slide } from "react-auto-image-slider";
 // import { Slide } from 'react-slideshow-image';
 import { useRef } from 'react';

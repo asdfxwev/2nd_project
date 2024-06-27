d021c72eb9d005bea3734435d3b86708a877f8ca
asdfxwev <167267637+asdfxwev@users.noreply.github.com>
2024-06-27 09:54:22 +0900

    as

---
+++ gundam/src/csc/Csc.js
@@ -0,0 +1,10 @@
+export default function Csc() {
+    return (
+        <>
+            <div className="CscListTitle">
+                <div style={{ width: '100px' }}></div>
+                <div style={{ width: '100px' }}></div>
+            </div>
+        </>
+    )
+}

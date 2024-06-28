97bd858621d8425d4ff30ea1208e15eb2a237ae9
Vaundy0824 <167267377+Vaundy0824@users.noreply.github.com>
2024-06-28 18:09:05 +0900

    6/28 주간 작업물

---
+++ gundam/src/cart/cart.jsx
@@ -0,0 +1,9 @@
+import './Cart.css';
+import ItemList from '../ItemList/ItemList';
+import ItemDataBase from '../ItemList/ItemDataBase';
+
+const cart = (() => {
+    return (
+        
+    );
+});

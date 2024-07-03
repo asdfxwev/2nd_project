b1597f8514f4f3eabc96f3daa68a0425f200b1a3
dydejr <167267452+dydejr@users.noreply.github.com>
2024-07-03 11:45:32 +0900

    카트수정

--- gundam/src/cart/cart.jsx
+++ gundam/src/cart/cart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './cart.css';
+import './Cart.css';
 import axios from 'axios';
 
 const CartItem = ({ item, onQuantityChange, onCheckboxChange, isChecked }) => {
